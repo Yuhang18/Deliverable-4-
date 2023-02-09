f6cc952dae2d6896885b82c5e0a8929c2dfb3847
87982 <87982@YUHANG>
2023-02-09 11:14:17 -0500

    Add project files.

---
+++ Deliverable (4)/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Author: Yuhang Bai
+Date: 2/9/2023
+Descrription: This C# console application provides practical examples of arrays
+*/
+
+using System;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < fibonacci.Length; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+
+            for (int i = 0; i < fibonacci.Length; i++)
+            {
+                Console.WriteLine("Fibonacci number ({0}) = {1}", i, fibonacci[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
